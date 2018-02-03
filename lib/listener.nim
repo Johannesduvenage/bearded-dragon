85801d988906a449f4c8834fae25af5596c4cf3e
jamesalbert <jimmychanga.cheese@gmail.com>
2018-02-02 22:47:00 -0800

    remove comments

--- lib/listener.nim
+++ lib/listener.nim
@@ -9,13 +9,3 @@ import clients/http/gdax.nim
 proc Listen* (service: string, asset: string): void =
   var gc = newGdaxHttpClient()
   gc.ListenTicker(service, asset)
-
-  # TODO: create database named <service>-<asset>-<time> if it doesn't exist
-
-  # let ws = waitFor newAsyncWebsocket("echo.websocket.org", Port 80, "/", ssl=false)
-
-
-
-  # TODO: every tick, insert new data into influx
-
-  # TODO: be able to view live updates from grafana
